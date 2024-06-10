85b43fc08cbf1cbd0f0a9d8ad69eba6880f33ea0
TTGFiregiraffe <firegiraffegaming@gmail.com>
2024-06-10 15:19:23 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace NoGravity
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.ttgfire.gorillatag.nogravity";
+        public const string Name = "NoGravity";
+        public const string Version = "1.0.0";
+    }
+}
